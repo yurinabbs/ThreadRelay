1ade748c69a2bcc3b704d023a0de5bdd6557db1f
yurinabbs <bianconi.yurinabil@franchettisalviani.net>
2026-04-14 10:58:07 +0200

    inizio implementazione progetto

---
+++ src/threadrelay/Corridore.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package threadrelay;
+
+/**
+ *
+ * @author bianconi.yurinabil
+ */
+public class Corridore implements Runnable{
+    
+    private boolean giocoAttivo;
+    
+}
